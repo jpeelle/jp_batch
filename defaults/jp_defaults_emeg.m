6efadca36a5c5f2fc47aac4f39e00ecd5a7e470a
Jonathan Peelle <jp01@lws004.(none)>
2009-07-31 15:18:29 +0100

    Added emeg defaults

--- defaults/jp_defaults_emeg.m
+++ defaults/jp_defaults_emeg.m
@@ -1,5 +1,5 @@
-function cfg = jp_defaults_aa(cfg)
-%JP_DEFAULTS_AA Default values for using AA scripts.
+function cfg = jp_defaults_emeg(cfg)
+%JP_DEFAULTS_EMEG Default values for using E/MEG scripts.
 
 % If no exisiting cfg, make a blank one
 if nargin < 1
@@ -7,11 +7,36 @@ if nargin < 1
 end
 
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% AA options
+% jp_ft_getdata
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-cfg.aap.options.autoidentifyfieldmaps = 1;
-cfg.aap.options.deletestructuralaftercopyingtocentralstore = 0;
-cfg.aap.options.fieldmapundistortversion = 'fieldmap_undistort_v403';
+cfg.jp_ft_getdata.heog_label = 'EEG061';
+cfg.jp_ft_getdata.veog_label = 'EEG062';
+cfg.jp_ft_getdata.ecg_label = 'EEG063';
 
+%cfg.jp_ft_getdata.analysisdir = [];
+
+cfg.jp_ft_getdata.trialfile = 1; % if 1, txt file with trial times required
+
+% set the following to define your own trials using fieldtrip
+cfg.jp_ft_getdata.triggers = [];
+cfg.jp_ft_getdata.prestim = [];
+cfg.jp_ft_getdata.poststim = [];
+
+cfg.jp_ft_getdata.veogreject = 0;
+cfg.jp_ft_getdata.veogreject_cfg = [];
+cfg.jp_ft_getdata.jumpreject = 0;
+cfg.jp_ft_getdata.jumpreject_cfg = [];
+
+% the preprocessing subfield gets passed in its entirety to
+% FieldTrip's PREPROCESSING function as the cfg option
+cfg.jp_ft_getdata.preprocessing.bpfilter = 'yes';
+cfg.jp_ft_getdata.preprocessing.bpfreq = [0.1 30]
+cfg.jp_ft_getdata.preprocessing.lpfilter = 'no'
+cfg.jp_ft_getdata.preprocessing.hpfilter = 'no'
+cfg.jp_ft_getdata.preprocessing.checkboundary = 0
+cfg.jp_ft_getdata.preprocessing.continuous = 'yes'
+cfg.jp_ft_getdata.preprocessing.channel = {'MEG'}
+cfg.jp_ft_getdata.preprocessing.blc = 'yes'
