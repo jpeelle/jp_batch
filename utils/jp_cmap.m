05b02025452482db0b1317ea1d3aee260fbc922a
Jonathan Peelle <peelle@gmail.com>
2011-03-08 17:33:07 -0500

    Added jp_cmap function for creating custom single-color maps.

---
+++ utils/jp_cmap.m
@@ -0,0 +1,28 @@
+function cmap = jp_cmap(c, nsteps)
+%JP_CMAP Create custom single-color colormap.
+%
+%  JP_CMAP(C) creates a custom colormap using C, which is an 1x3 vector
+%  indicating a Matlab color.
+%
+%  JP_CMAP(C,NSTEPS) creates the colormap with NSTEPS numbe rof steps.
+%  If not specified, 64 steps are used.
+%
+%  Example:
+%
+%    red = jp_cmap([1 0 0]);
+
+% Jonathan Peelle
+
+if nargin < 2 || isempty(nsteps)
+  nsteps = 64;
+end
+
+if size(c,1) > 1 || size(c,2) ~= 3
+  error('C must be a 1 by 3 vector indicating a Matlab color.');
+end
+
+cmap = repmat(c, nsteps, 1);
+
+
+
+
