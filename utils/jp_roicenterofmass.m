41882a90daf9b1932575ca121ef4c0e9837f37f6
Jonathan Peelle <peelle@gmail.com>
2012-02-12 14:16:18 +0000

    Updated jp_roicenterofmass to handle multiple input images

--- utils/jp_roicenterofmass.m
+++ utils/jp_roicenterofmass.m
@@ -1,9 +1,14 @@
-function com = jp_roicenterofmass(img)
+function com = jp_roicenterofmass(img, list)
 %JP_ROICENTEROFMASS Find the XYZ coordinate at the 'center' of an ROI.
 %
 % JP_ROICENTEROFMASS(IMG) finds the mean location across all non-zero X,Y,
 % and Z axes, in mm (using transformations from the image headers).  Result
 % is rounded to the nearest mm.
+%
+% If IMG is not specified, you are prompted to select one ore more images.
+%
+% JP_ROICENTEROFMASS(IMG, LIST) will print a list of images and coordinates
+% to the display window.
 %
 % JP_ROICENTEROFMASS depends on some helper functions from recent versions
 % of SPM.
@@ -11,9 +16,40 @@ function com = jp_roicenterofmass(img)
 % Jonathan Peelle
 % University of Pennsylvania
 
-if nargin < 1 || isempty(img)
-  img = spm_select(1, 'Image');
+if nargin < 2 || isempty(list)
+  list = 0;
 end
+
+if nargin < 1 || isempty(img)
+  img = spm_select(Inf, 'Image');
+end
+
+
+% make sure img is in the expected format (e.g., if passed as cell array)
+if iscell(img)
+  img = strvcat(img);
+end
+
+nimg = size(img,1); % how many images?
+com = zeros(nimg,3); % hold all centers of mass
+
+for i=1:nimg
+  com(i,:) = findcom(deblank(img(i,:)));
+
+  if list > 0
+    fprintf('%s:\t%4i\t%4i\t%4i\n', img(i,:), com(i,1), com(i,2), com(i,3));
+  end
+end
+
+if list > 0
+  fprintf('\n\n');
+end
+
+
+end % main function
+
+
+function com = findcom(img)
 
 V = spm_vol_nifti(img);
 [Y, XYZ] = spm_read_vols(V);
@@ -22,8 +58,9 @@ Y = reshape(Y, 1, numel(Y));
 % Only look at indices for non-zero voxels
 x = Y>0;
 
+% If no nonzeros voxels, warn, otherwise, take mean of what was found.
 if sum(x)==0
-  fprintf('Warning: No voxels > 0 in image.');
+  fprintf('Warning: No voxels > 0 in image %s.\n', img);
   com = nan(1,3);
 else
   XYZx = XYZ(:,Y>0);
@@ -35,3 +72,5 @@ end
 if size(com,1)==3
   com = com';
 end
+
+end % findcom subfunction
